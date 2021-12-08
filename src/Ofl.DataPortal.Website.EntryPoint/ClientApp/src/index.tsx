1adc030855563b72c74f33c7a499d26c1976f116
casperOne <casperOne@caspershouse.com>
2021-12-08 13:50:12 -0500

    Cleanup.

--- src/Ofl.DataPortal.Website.EntryPoint/ClientApp/src/index.tsx
+++ src/Ofl.DataPortal.Website.EntryPoint/ClientApp/src/index.tsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 // Service worker.
 import { unregister } from './serviceWorker';
 
-// Support needed across entire app.
-import 'semantic-ui-css/semantic.min.css';
-import 'flexboxgrid/dist/flexboxgrid.min.css'
-import './layout.css'
-import "normalize.css/normalize.css";
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-
 // Get the base URL and the root element where
 // the app is hosted.
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
